7a2b526207dd8b1cd97482a1ee93b57e1235303d
gayeon <gy_h19@naver.com>
2023-10-10 17:48:56 +0900

    1010

--- 1006test2/test.js
+++ 1006test2/test.js
@@ -2,7 +2,7 @@ function pwd(){
     const userId = document.getElementById("userId");
 
     const area3 = document.getElementById("area3");
-    area3.innerHTML = "영문자 대/소문자 특수문자, 숫자 포함 8 ~ 32자";
+    area3.innerHTML = "영문자 대/소문자 특수문자, 숫자 포함 8~32자";
 
     const password = document.getElementById("password");
     if(userId.value.replace(/ /g,"") === ''){
@@ -43,4 +43,22 @@ function check(){
         const area4 = document.getElementById("area4");
         area4.innerHTML = "필수 입력 항목 입니다";
     }
+}
+
+function checkArea(){
+    const userId = document.getElementById("userId");
+    const area1 = document.getElementById("area1");
+    if(userId.value.replace(/ /g,"") !== ''){
+        area1.innerHTML = "";
+    }
+    const password = document.getElementById("password");
+    const area2 = document.getElementById("area2");
+    if(password.value.replace(/ /g,"") !== ''){
+        area2.innerHTML = "";
+    }
+    const confirmPassword = document.getElementById("confirmPassword");
+    const area4 = document.getElementById("area4");  
+    if(confirmPassword.value.replace(/ /g,"") !== ''){
+        area4.innerHTML = "";
+    } 
 }
--- todolist_copy/index.js
+++ todolist_copy/index.js
@@ -1,22 +1,48 @@
-function addTodo(){
-    const todo = document.getElementById("search-input");
-    
-    if(todo.value === ''){
+const todoList = [];
+
+function addTodo(ev){
+
+    if(document.getElementById('search-input').value.trim() === ''){
         return;
     }
 
-    const todoListUl = document.getElementsByClassName('todo-list')[0];
-    const toDoLi = document.createElement('li');
-    toDoLi.innerHTML = todo.value;
-    todoListUl.appendChild(toDoLi);
-    todo.value = '';
+    //추가할 일정 요소 만들기
+    function getTodoNode(){
+        //내가 작성한 일정명 가지고 오기
+        const todoTitle = document.getElementById("search-input").value;
 
-    const removeToDoLi = document.createElement('div');
-    removeToDoLi.className = 'todo-remove-btn';
-    removeToDoLi.innerHTML = '<i class="fa-solid fa-eraser"></i>';
-    toDoLi.appendChild(removeToDoLi);
+        //todo요소 만들고 붙여주기
+        const todoNode = document.createElement('div');
+        todoNode.innerText = todoTitle;
+        todoNode.onclick = function(){
+            todoNode.isDone = !todoNode.isDone;
+            if(this.classList.length === 0){
+                this.className = 'done';
+            } else {
+                this.className = '';
+            }
+        }
 
-    removeToDoLi.onclick = function(ev){
-        
+        //요소 삭제버튼 만들고 붙여주기
+        const removeBtn = document.createElement('button');
+        removeBtn.className = "remove-btn";
+        removeBtn.innerHTML = '<i class="fa-solid fa-eraser"></i>';
+        todoNode.appendChild(removeBtn);
+        removeBtn.onclick = function(){
+            this.parentNode.remove();
+        }
+
+        document.getElementById('search-input').value = '';
+
+        return todoNode;
+    }
+
+    //엔터키를 입력했다면 해당코드 실행
+    if(ev.keyCode === 13 || !ev.keyCode){
+    //일정을 보여주는 영역
+    const todoListView = document.getElementById("todo-list-view");
+    //요소 생성하는 함수 호출
+    const todoNode = getTodoNode();
+    todoListView.appendChild(todoNode);
     }
 }
